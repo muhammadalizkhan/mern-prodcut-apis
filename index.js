7d1802b245a60e7e83748650f5483c0e0eea0483
Muhamad Ali Khan <ali7847khan@gmail.com>
2024-12-13 12:20:49 +0500

    product

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js')
+
 console.log('Hello, world!');
 const app = express();
 
@@ -11,9 +13,10 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/api/products', (req, res) => {
+app.post('/api/products', async (req, res) => {
     try {
-
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
